05b8780611ebc7d9848d6cfe5950f89c1418243d
Ksenia1227 <krylivaksenia13@gmail.com>
2024-02-27 22:44:02 +0500

    ini

--- lib/app/modules/task33/views/task33_view.dart
+++ lib/app/modules/task33/views/task33_view.dart
@@ -6,63 +6,64 @@ import '../controllers/task33_controller.dart';
 
 class Task33View extends GetView<Task33Controller> {
   //  const Task33View({Key? key}) : super(key: key);
-   @override
+  @override
   final controller = Get.put(Task33Controller());
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 48, 72, 138),
+        backgroundColor: Color.fromARGB(255, 48, 72, 138),
         appBar: AppBar(
-          
-          title: Text("Игра"),
+          title: Text("Игра: Крeстики и Нолики"),
+          centerTitle: true,
         ),
-         body: 
-         Column(
-          children: [
-              Padding(
-              padding: EdgeInsets.all(25.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-           children: [Text("Ход: ",
-           style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),),
-            Obx(() => Text(
-                    controller.currentPlayer.value == "X"
-                        ? "O"
-                        : "X",
-                    style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: controller.currentPlayer.value == "X"
-                            ? Colors.amber
-                            : Colors.blue))),
-          
-          ]),
-             ),
-            Container(
-
-                width: 400,
-                height: 400,
-                child: GridView.builder(
-                    itemCount: controller.changeXO.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 3),
-                    itemBuilder: (BuildContext ctx, int index) {
-                      return GestureDetector(
-                        onTap: () {controller.makeMove(index);},
-                        child: Container(
-                          decoration: BoxDecoration(
-                            
-                              border: Border.all(color: Color.fromARGB(255, 0, 0, 0))),
-                          child: Center(
-                              child: Obx(() =>Text(
-                            controller.changeXO[index],
-                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 90.0, color: Colors.amber),
-                          ))),
+        body: Column(children: [
+          Padding(
+            padding: EdgeInsets.all(25.0),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(
+                "Ход: ",
+                style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Obx(() => Text(controller.currentPlayer.value == "X" ? "X" : "O",
+                  style: const TextStyle(
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red))),
+            ]),
+          ),
+          Container(
+              width: 400,
+              height: 400,
+              child: GridView.builder(
+                  itemCount: controller.changeXO.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemBuilder: (BuildContext ctx, int index) {
+                    return GestureDetector(
+                      onTap: () {
+                        controller.makeMove(index);
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.blueAccent,
+                            border: Border.all(
+                                color: Color.fromARGB(255, 0, 0, 0), width: 5)),
+                        child: Center(
+                          child: Obx(() => Text(
+                                controller.changeXO[index],
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 90.0,
+                                    color: Colors.amber),
+                              )),
                         ),
-                      );
-                    })),
-    ]));
+                      ),
+                    );
+                  })),
+        ]));
   }
 }
